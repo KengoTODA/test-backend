8286c88fa3d3ab898037cde2ffe39e04108ded43
Kengo TODA <skypencil@gmail.com>
2021-03-28 09:01:51 +0800

    test: make user service test runnable

--- src/application/auth.service.spec.ts
+++ src/application/auth.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UserRepository } from '../domain/user.repository';
+import { OnMemoryUserRepository } from '../infra/on-memory/user.repository';
 import { AuthService } from './auth.service';
+import { UserAppService, UserDomainService } from './user.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
+    const userRepositoryProvider = {
+      provide: UserRepository,
+      useClass: OnMemoryUserRepository,
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        userRepositoryProvider,
+        AuthService,
+        UserAppService,
+        UserDomainService,
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
